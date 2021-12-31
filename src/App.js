163d73a4801e0e7675d81c4d509c0d125047b6ae
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-12-31 13:29:34 +0300

    feed component header complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import Feed from "./components/Feed/Feed";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
+import faker from "faker";
+import { AvatarGenerator } from "random-avatar-generator";
+// generate random avatar
+const generator = new AvatarGenerator();
 
 function App() {
   return (
@@ -10,8 +14,17 @@ function App() {
         <aside className=" hidden sm:grid  sm:col-span-1 h-[89vh] overflow-x-hidden overflow-y-scroll sticky top-[11vh] ">
           <Sidebar />
         </aside>
-        <section className="col-span-3 sm:col-span-2 bg-slate-100 ">
-          <Feed />
+        <section className="col-span-3 sm:col-span-2  ">
+          <Feed
+            userImage={generator.generateRandomAvatar("nazuu")}
+            username={faker.name.findName()}
+            displayName={faker.name.findName()}
+            postSong="sonido original - ortegaortega786"
+            postVideo="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            postLikes={faker.random.number()}
+            postComments={faker.random.number()}
+            postShares={faker.random.number()}
+          />
         </section>
       </section>
     </main>
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,8 +1,45 @@
+import { MusicNoteIcon } from "@heroicons/react/solid";
 
-export default function Feed() {
+export default function Feed({
+  userImage,
+  displayName,
+  username,
+  postSong,
+  postVideo,
+  postLikes,
+  postComments,
+  postRetweets,
+}) {
   return (
-    <div>
-      feed here
-    </div>
-  )
+    <section className="p-2">
+      <header>
+        <div className=" flex justify-between">
+          <section className="flex items-center justify-start py-2 ">
+            <div className="h-10 w-10 rounded-full border-[1px solid red]">
+              <img src={userImage} alt="" />
+            </div>
+            <div className=" ml-2">
+              <h1 className="cursor-pointer hover:underline">
+                {username}{" "}
+                <span className="text-xs text-gray-500">{displayName}</span>
+              </h1>
+
+              <p className="text-sm font-bold mt-2 cursor-pointer hover:underline text-gray-500 flex items-center ">
+                <MusicNoteIcon className="h-4 w-4 mr-1 text-black  " />
+                {postSong}
+              </p>
+            </div>
+          </section>
+          <section className="flex">
+            <button className="border border-red-500 text-[red] h-10  px-4  rounded-sm">
+              Follow
+            </button>
+          </section>
+        </div>
+        <div></div>
+        <div></div>
+      </header>
+      <main></main>
+    </section>
+  );
 }
