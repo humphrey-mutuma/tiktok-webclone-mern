fa6243f76e03ad38a3e5cbbdf7127e5d38c4ec73
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-01-02 09:14:06 +0300

    add video likes, shares, comments

--- src/App.js
+++ src/App.js
@@ -19,8 +19,28 @@ function App() {
             userImage={generator.generateRandomAvatar("nazuu")}
             username={faker.name.findName()}
             displayName={faker.name.findName()}
-            postSong="sonido original - ortegaortega786"
-            postVideo="https://v16-webapp.tiktok.com/4053ba282100e6d7a4f166e0c8cd3b4b/61cf19f2/video/tos/alisg/tos-alisg-pve-0037c001/60803bc08c4641c8928c00d854d59da7/?a=1988&br=428&bt=214&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=1&er=&ft=Yu12_FBqkag3-I&l=2021123108545901024500502712F7CD03&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amk8OWU6Zjh5ODMzODczNEApOGc2OGY7NGVnNztkOzlpOGdxZjM2cjRnZG1gLS1kMS1zc14yXjQxLzQ2My9jM2MxMjU6Yw%3D%3D&vl=&vr="
+            postSong="99 problems but a react ain't one"
+            postVideo="https://v16-webapp.tiktok.com/ebe60415c278f561eb7ec15c90f2829d/61d0ba2d/video/tos/alisg/tos-alisg-pve-0037c001/85b2004f059a40b19abf93430b20c364/?a=1988&br=4964&bt=2482&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FBqkag3-I&l=2022010114311701024504302602F959BF&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3czOTU6ZjQ2OjMzODczNEApO2k5OGg1PGU7NzQ5O2ZmM2diNGNzcjRnbjVgLS1kMS1zczI0XzExNGAvXjNeX2FeLjY6Yw%3D%3D&vl=&vr="
+            postLikes={faker.random.number()}
+            postComments={faker.random.number()}
+            postShares={faker.random.number()}
+          />
+          <Feed
+            userImage={generator.generateRandomAvatar("jsude")}
+            username={faker.name.findName()}
+            displayName={faker.name.findName()}
+            postSong="original sound - Moods"
+            postVideo="https://v16-webapp.tiktok.com/a463d390153e5ec5de4b604e01ce0fe6/61d0c6b3/video/tos/alisg/tos-alisg-pve-0037c001/48754f7fd50c4132b766cc2dcb6b6f25/?a=1988&br=3324&bt=1662&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FBqkag3-I&l=202201011524530102450050272109174D&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajppaTc6ZjNwOTMzODczNEApM2U5Z2dpNWRlNzlmPDtpOmdjc21fcjQwYXFgLS1kMS1zczAvNi9gYjViXi8uXi82YTY6Yw%3D%3D&vl=&vr="
+            postLikes={faker.random.number()}
+            postComments={faker.random.number()}
+            postShares={faker.random.number()}
+          />
+          <Feed
+            userImage={generator.generateRandomAvatar("nazuu")}
+            username={faker.name.findName()}
+            displayName={faker.name.findName()}
+            postSong="Kirikou Logobi Remix - Mia"
+            postVideo="https://v16-webapp.tiktok.com/c092be0573422ee4a51aff35ef9a87a5/61d0c6b8/video/tos/alisg/tos-alisg-pve-0037c001/fc1bdb473cc34c1d87ede3e0c9f1d469/?a=1988&br=4164&bt=2082&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=Yu12_FBqkag3-I&l=202201011524530102450050272109174D&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amtyZjo6Zjw0OTMzODczNEApOTg2ZDY0OWVmNzc7ZDY7Omc2bC4vcjQwYmtgLS1kMS1zczUwM18wNV8tNi8xLS0tLTQ6Yw%3D%3D&vl=&vr="
             postLikes={faker.random.number()}
             postComments={faker.random.number()}
             postShares={faker.random.number()}
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,6 +1,12 @@
 import { MusicNoteIcon } from "@heroicons/react/solid";
 import { HeartIcon } from "@heroicons/react/outline";
-import { ChatIcon, ReplyIcon } from "@heroicons/react/solid";
+import {
+  ChatIcon,
+  ReplyIcon,
+  HeartIcon as HeartIcon2,
+} from "@heroicons/react/solid";
+import Ticker from "react-ticker";
+import React, { useRef, useState } from "react";
 
 export default function Feed({
   userImage,
@@ -12,24 +18,41 @@ export default function Feed({
   postComments,
   postShares,
 }) {
+  const [likes, setLikes] = useState(false);
+  const [comments, setComments] = useState(false);
+  const [shares, setShares] = useState(false);
+  const likesref = useRef(postLikes);
+
   return (
-    <section className="p-2">
+    <section className="m-2 p-2 border-b">
       <header>
         <div className=" flex justify-between">
-          <section className="flex items-center justify-start py-2 ">
-            <div className="h-10 w-10 rounded-full border-[1px solid red]">
+          <section className="flex  items-center justify-start py-2 flex-1 ">
+            <div className="h-10 w-10 bg-black rounded-full border-[1px solid red]">
               <img src={userImage} alt="" />
             </div>
-            <div className=" ml-2">
+            <div className=" ml-2 w-3/4 lg:w-1/2  ">
               <h1 className="cursor-pointer hover:underline">
                 {username}{" "}
                 <span className="text-xs text-gray-500">{displayName}</span>
+                <br />
+                <p className="text-xs text-gray-500 my-1">
+                  another TikTok #dance ! #flipbook
+                </p>
               </h1>
 
-              <p className="text-sm font-bold mt-2 cursor-pointer hover:underline text-gray-500 flex items-center ">
-                <MusicNoteIcon className="h-4 w-4 mr-1 text-black  " />
-                {postSong}
-              </p>
+              <div className="flex w-full items-center ">
+                <MusicNoteIcon className=" h-4 w-4 mr-1 text-black  " />
+                <div className="flex-1">
+                  <Ticker mode="smooth">
+                    {({ index }) => (
+                      <>
+                        <h1>{postSong}</h1>
+                      </>
+                    )}
+                  </Ticker>
+                </div>
+              </div>
             </div>
           </section>
           <section className="flex">
@@ -41,7 +64,7 @@ export default function Feed({
       </header>
       <main className="p-2 flex">
         <section className=" ml-10 h-[76vh] w-[18rem] rounded-lg">
-          <video className="w-full h-full rounded-lg object-fill" controls>
+          <video loop className="w-full h-full rounded-lg object-fill" controls>
             <source src={postVideo} type="video/mp4" />
             <source src={postVideo} type="video/ogg" />
             Your browser does not support HTML video.
@@ -50,22 +73,55 @@ export default function Feed({
         <section className=" ml-3 flex items-end  justify-center">
           <div className="mb-5">
             <span className="grid place-items-center">
-              <div className=" h-10  w-10 rounded-full p-1 bg-gray-200 ">
-                <HeartIcon className=" h-full w-full   text-[red]  " />
-              </div>
-              <p className="text-sm text-gray-500">{postLikes}</p>
+              {likes ? (
+                <div
+                  onClick={(e) => setLikes(!likes)}
+                  className=" cursor-pointer h-10  w-10 rounded-full p-1 bg-gray-200 hover:bg-gray-300 "
+                >
+                  <HeartIcon2 className=" h-full w-full   text-[red]  " />
+                </div>
+              ) : (
+                <div
+                  onClick={(e) => setLikes(!likes)}
+                  className=" cursor-pointer h-10  w-10 rounded-full p-1 bg-gray-200 hover:bg-gray-300"
+                >
+                  <HeartIcon className=" h-full w-full   text-[red]  " />
+                </div>
+              )}
+
+              <p className="text-sm text-gray-500">
+                {likes ? postLikes + 1 : postLikes}
+              </p>
             </span>
             <span className="grid place-items-center">
-              <div className=" h-10  w-10 rounded-full p-1 bg-gray-200 ">
-                <ChatIcon className=" h-full w-full   text-black  " />
+              <div
+                onClick={(e) => setComments(!comments)}
+                className=" cursor-pointer h-10  w-10 rounded-full p-1 bg-gray-200 hover:bg-gray-300"
+              >
+                <ChatIcon
+                  className={` h-full w-full  ${
+                    comments ? "text-blue-800" : "text-black"
+                  }   `}
+                />
               </div>
-              <p className="text-sm text-gray-500">{postComments}</p>
+              <p className="text-sm text-gray-500">
+                {comments ? postComments + 1 : postComments}
+              </p>
             </span>
-            <span className="grid place-items-center">
-              <div className=" h-10  w-10 rounded-full p-1 bg-gray-200 ">
-                <ReplyIcon className=" h-full w-full   text-black  " />
+            <span className="grid place-items-center ">
+              <div
+                onClick={(e) => setShares(!shares)}
+                className=" cursor-pointer h-10  w-10 rounded-full p-1 bg-gray-200 hover:bg-gray-300 "
+              >
+                <ReplyIcon
+                  className={` h-full w-full  ${
+                    shares ? "text-green-800" : "text-black"
+                  }   `}
+                />
               </div>
-              <p className="text-sm text-gray-500">{postShares}</p>
+              <p className="text-sm text-gray-500">
+                {shares ? postShares + 1 : postShares}
+              </p>
             </span>
           </div>
         </section>
